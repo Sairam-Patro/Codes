722d2e3da8dc1764213e1d3db2b2d7b3cdffb7bb
A Sairam patro <152791589+Sairam-Patro@users.noreply.github.com>
2026-06-27 16:49:39 +0530

    Time: 0 ms (100%), Space: 9.6 MB (92.5%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+
+        vector<int> freq(26,0);
+
+        for(char i : s) freq[i - 'a']++;
+
+        for(char i : t) freq[i - 'a']--;
+
+        for(int i : freq) if(i != 0) return false;
+
+        return true;
+    }
+};
